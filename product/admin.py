189d4a6ced43b4ab3575eacb9b4ac70079adb091
Jahid Alam <jahidalam01741@gmail.com>
2026-02-15 20:57:07 -0800

    iamge field add

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from product.models import Product, Category, Review, ProductImage,Booking
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(Booking)
+
